f37525c34c01faa6a71559a2b91c5db324bd75d9
DevTamakuwala <dev.official2424@gmail.com>
2025-06-13 00:01:41 +0530

    Completed the database connection with H2 database and PostgreSQL. also the service layer and repository layer. Completed till video 194.

--- src/main/java/com/springbootcourse/app/SpringBootCourseApplication.java
+++ src/main/java/com/springbootcourse/app/SpringBootCourseApplication.java
@@ -1,28 +1,42 @@
 package com.springbootcourse.app;
 
-import com.springbootcourse.app.model.Alien;
-import com.springbootcourse.app.model.Laptop;
-import com.springbootcourse.app.service.LaptopService;
+import com.springbootcourse.app.model.Student;
+import com.springbootcourse.app.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootCourseApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringBootCourseApplication.class, args);
-        System.out.println("Hello world");
+/*        System.out.println("Hello world");
 
-//        Alien alien = context.getBean(Alien.class);
-//        System.out.println(alien.getAge());
-//        alien.code();
+        Alien alien = context.getBean(Alien.class);
+        System.out.println(alien.getAge());
+        alien.code();
 
         LaptopService service = context.getBean(LaptopService.class);
 
         Laptop laptop = context.getBean(Laptop.class);
-        service.addLaptop(laptop);
+        service.addLaptop(laptop);*/
 
+
+        Student student = context.getBean(Student.class);
+
+        student.setRollNo(104);
+        student.setName("John Doe");
+        student.setMarks(78);
+
+        StudentService service = context.getBean(StudentService.class);
+
+        service.addStudent(student);
+
+        List<Student> students = service.getStudents();
+        System.out.println(students);
     }
 
 }
---
+++ src/main/java/com/springbootcourse/app/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.springbootcourse.app.service;
+
+import com.springbootcourse.app.model.Student;
+import com.springbootcourse.app.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private StudentRepository repository;
+
+    public StudentRepository getRepository() {
+        return repository;
+    }
+
+    @Autowired
+    public void setRepository(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addStudent(Student student){
+        repository.save(student);
+    }
+
+    public List<Student> getStudents() {
+        return repository.findAll();
+    }
+}
