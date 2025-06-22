5807764a6f5ca9efbd93f8638f2c24b060a4fdfc
DevTamakuwala <dev.official2424@gmail.com>
2025-06-22 10:48:14 +0530

    Configured spring boot with jsp complete till video 213.

--- src/main/java/com/example/springbootcourse/HomeController.java
+++ src/main/java/com/example/springbootcourse/HomeController.java
@@ -17,6 +17,8 @@ public class HomeController {
     public String add(@RequestParam("num1") int num1,@RequestParam("num2")  int num2, Model model) {
         int result = num1 + num2;
         model.addAttribute("result", result);
+
+
         return "result";
     }
 }
