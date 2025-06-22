777fb8ff2138e2c5a6d25359e92fb42e7b70df52
DevTamakuwala <dev.official2424@gmail.com>
2025-06-22 10:45:24 +0530

    Configured spring boot with jsp.

---
+++ src/main/java/com/example/springbootcourse/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.springbootcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1,@RequestParam("num2")  int num2, Model model) {
+        int result = num1 + num2;
+        model.addAttribute("result", result);
+        return "result";
+    }
+}
